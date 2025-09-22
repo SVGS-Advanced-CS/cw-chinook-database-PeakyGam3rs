f4a49f5da009c1fcc0f2d037ab5bb6a0e164329e
PeakyGam3rs <124310914+PeakyGam3rs@users.noreply.github.com>
2025-09-22 10:26:36 -0400

    working query

--- src/main/java/com/svgs/Main.java
+++ src/main/java/com/svgs/Main.java
@@ -1,10 +1,31 @@
 package com.svgs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 
 public class Main {
     public static void main(String[] args) {
         String url = "jdbc:sqlite:./src/main/resources/chinook.db";
 
+        try {
+            Connection conn = DriverManager.getConnection(url);
+
+            Statement state = conn.createStatement();
+            String query = "SELECT * FROM employees";
+            ResultSet results = state.executeQuery(query);
+
+            while (results.next()) {
+                String firstName = results.getString("FirstName");
+                String lastName = results.getString("LastName");
+                System.out.println("First Name: " + firstName + "Last Name: " + lastName);
+            }
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
     }
 }
