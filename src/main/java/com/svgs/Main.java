9a62abe24a197e4c15cc7e990c33570e80764b12
PeakyGam3rs <124310914+PeakyGam3rs@users.noreply.github.com>
2025-09-23 17:05:36 -0400

    more stuff

--- src/main/java/com/svgs/Main.java
+++ src/main/java/com/svgs/Main.java
@@ -3,16 +3,19 @@ package com.svgs;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Scanner;
 
 
 public class Main {
+    private static Connection conn;
     public static void main(String[] args) {
-        String url = "jdbc:sqlite:./src/main/resources/chinook.db";
+        createDatabase();
+        String url = "jdbc:sqlite:./src/main/resources/users.db";
 
         try {
-            Connection conn = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection(url);
             Statement state = conn.createStatement();
-            state.executeUpdate("DELETE FROM employees WHERE employeeId>=11 AND FirstName=='Anthony' AND LastName=='Tyler'");
+            //state.executeUpdate("DELETE FROM employees WHERE employeeId>=11 AND FirstName=='Anthony' AND LastName=='Tyler'");
             //state.executeUpdate("INSERT INTO employees(FirstName, LastName, EmployeeId) VALUES('Anthony', 'Tyler', 9)");
             /* 
             Statement state = conn.createStatement();
@@ -29,6 +32,42 @@ public class Main {
         } catch (Exception e) {
             System.out.println(e);
         }
+
+        mainMenu();
+
+    }
+
+    public static void createDatabase() {
+        String url = "jdbc:sqlite:./src/main/resources/users.db";
+
+        try {
+            conn = DriverManager.getConnection(url);
+            Statement state = conn.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS users(username TEXT PRIMARY KEY, password TEXT NOT NULL, role TEXT);";
+            state.execute(sql);
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void mainMenu() {
+        System.out.println("1. Log in");
+        System.out.println("2. Create User");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
+        Scanner input = new Scanner(System.in);
+        String choice = input.nextLine();
+        if (choice.equals("1")) {
+            
+        } else if (choice.equals("2")) {
+            
+        } else if (choice.equals("3")) {
+            return;
+        } else {
+            System.out.println("Invalid choice");
+        }
+        mainMenu();
 
     }
 }
